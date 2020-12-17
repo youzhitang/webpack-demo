cc4cb8bb6b6fd6237f8a8a4fe8a10de3ceba3c0e
tangyou <maoyou5749@163.com>
2020-12-17 17:35:25 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path')
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'index.[contenthash].js'
-    },
-    plugins: [new HtmlWebpackPlugin({
-        title: '釉纸糖',
-        //使用template传递自定义HTML文件
-        template: 'src/assets/index.html'
-    })],
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-        ],
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'index.[contenthash].js'
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: '釉纸糖',
+    //使用template传递自定义HTML文件
+    template: 'src/assets/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
+    ],
+  },
 }
 
 
